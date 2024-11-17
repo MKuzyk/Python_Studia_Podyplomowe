761c3069eed9cdd2fc009b1326e70610c3827f96
Michał Kuzyk <g.michalkuzyk@gmail.com>
2024-11-17 17:38:48 +0100

    commit

---
+++ Zad_dodatkwe/Zad_dod_1.py
@@ -0,0 +1,33 @@
+'''
+Zadanie 1
+Napisz program obliczający wartość bezwzględną z liczby podanej przez użytkownika.
+'''
+
+podana_liczba = int(input("podaj liczbę. W wyniku dostaniesz liczbę bezwzględną! "))
+
+print("wartość bezwzględna z poodanej liczby wynosi :",abs(podana_liczba))
+
+'''
+Zadanie 2
+Napisz program informujący czy liczba podana przez użytkownika jest większa, mniejsza czy równa zero. Wykorzystaj tylko dwie instrukcje warunkowe.
+'''
+
+'''
+Zadanie 3
+Napisz program informujący czy liczba podana przez użytkownika jest parzysta czy nieparzysta.
+'''
+
+'''
+Zadanie 4
+Napisz program wyznaczający najmniejszą z trzech liczb podanych przez użytkownika.
+'''
+
+'''
+Zadanie 5
+Napisz program, który odpowiada na pytanie, czy wśród trzech liczb są choć dwie takie same.
+'''
+
+'''
+Zadanie 6
+Napisz program, który odpowiada na pytanie, czy trzy podawane liczby całkowite są ustawione w porządku rosnącym.
+'''
