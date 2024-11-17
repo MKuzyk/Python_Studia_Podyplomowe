ea72d01a015aa3084f0c0ce28fd659a091689e0c
Michał Kuzyk <g.michalkuzyk@gmail.com>
2024-11-17 18:06:05 +0100

    commit

--- Zad_dodatkwe/Zad_dod_1.py
+++ Zad_dodatkwe/Zad_dod_1.py
@@ -38,12 +38,41 @@ Zadanie 4
 Napisz program wyznaczający najmniejszą z trzech liczb podanych przez użytkownika.
 '''
 
+liczba_1=int(input("podaj pierwszą liczbę: "))
+liczba_2=int(input("podaj drugą liczbę: "))
+liczba_3=int(input("podaj trzecią liczbę: "))
+
+if liczba_1>liczba_2 and liczba_1>liczba_3:
+    print(liczba_1," jest największa w zbiorze podanych liczb")
+elif liczba_2>liczba_1 and liczba_2>liczba_3:
+    print(liczba_2, " jest największa w zbiorze podanych liczb")
+else:
+    print(liczba_3, " jest największa w zbiorze podanych liczb")
+
 '''
 Zadanie 5
 Napisz program, który odpowiada na pytanie, czy wśród trzech liczb są choć dwie takie same.
 '''
 
+liczba_1=int(input("podaj pierwszą liczbę: "))
+liczba_2=int(input("podaj drugą liczbę: "))
+liczba_3=int(input("podaj trzecią liczbę: "))
+
+if liczba_1==liczba_2 or liczba_2==liczba_3 or  liczba_1==liczba_3:
+    print("w zbiorze podanych liczb dwie są takie same")
+else:
+    print("w podanych zbiorze nie ma dwóch takich samych liczb")
+
 '''
 Zadanie 6
 Napisz program, który odpowiada na pytanie, czy trzy podawane liczby całkowite są ustawione w porządku rosnącym.
 '''
+
+liczba_1=int(input("podaj pierwszą liczbę: "))
+liczba_2=int(input("podaj drugą liczbę: "))
+liczba_3=int(input("podaj trzecią liczbę: "))
+
+if liczba_1<liczba_2 and liczba_2<liczba_3:
+    print("liczby sa ustawione w porządku rosnącym")
+else:
+    print("liczby nie sa ustawione w porządku rosnącym")
