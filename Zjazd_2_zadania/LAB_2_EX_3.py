0dd6ee4fa2246903006b7d25e60142e10cb0a193
Michał Kuzyk <g.michalkuzyk@gmail.com>
2024-11-17 17:03:09 +0100

    New file added to the Python_Z_2

---
+++ Zjazd_2_zadania/LAB_2_EX_3.py
@@ -0,0 +1,3 @@
+#Napisz grę w zgadywanie liczby z przedziału od 1 do 10. Gracz powinien dostać
+#3 szanse. Po nieudanej próbie, gracz powinien otrzymać podpowiedź np.
+#o parzystości zgadywanej liczby itp..
