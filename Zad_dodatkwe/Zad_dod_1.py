f9868f80181b60ba9c9a7b25228e1eb15348d7ec
Michał Kuzyk <g.michalkuzyk@gmail.com>
2024-11-17 17:48:21 +0100

    commit

--- Zad_dodatkwe/Zad_dod_1.py
+++ Zad_dodatkwe/Zad_dod_1.py
@@ -9,13 +9,29 @@ print("wartość bezwzględna z poodanej liczby wynosi :",abs(podana_liczba))
 
 '''
 Zadanie 2
-Napisz program informujący czy liczba podana przez użytkownika jest większa, mniejsza czy równa zero. Wykorzystaj tylko dwie instrukcje warunkowe.
+Napisz program informujący czy liczba podana przez użytkownika jest większa, mniejsza czy równa zero. Wykorzystaj
+ tylko dwie instrukcje warunkowe.
 '''
+
+podana_liczba = int(input("podaj liczbę: "))
+
+if podana_liczba>0:
+    print("liczba ",podana_liczba," jest większa od zera")
+elif podana_liczba<0:
+    print("liczba ", podana_liczba, " jest mniejsza od zera")
+else: print("padana liczba jest równa 0")
 
 '''
 Zadanie 3
 Napisz program informujący czy liczba podana przez użytkownika jest parzysta czy nieparzysta.
 '''
+
+podana_liczba = int(input("podaj liczbę: "))
+
+if podana_liczba%2==0:
+    print("podana liczba jest parzysta!!")
+else:
+    print("podana liczba jest nieparzysta!")
 
 '''
 Zadanie 4
