2dda0ae06cbf068b1593b8c0902d330474537caf
Michał Kuzyk <g.michalkuzyk@gmail.com>
2024-12-01 10:55:55 +0100

    commit

--- Zjazd_2_zadania/LAB_9_EX_1.py
+++ Zjazd_2_zadania/LAB_9_EX_1.py
@@ -3,12 +3,15 @@ Wyświetl tylko liczby podzielne przez 3, 5 lub 7, ze zbioru liczb z zakresu
 określonego przez użytkownika.
 '''
 
-zakres=int(input("Podaj zakres od 0 do:"))
+zakres_from=int(input("Podaj początkową liczbę zakresu:"))
+zakres_to=int(input("Podaj końcową liczbę zakresu:"))
 
-for i in range (zakres+1):
+print("liczby podziene przez 3, 5 lub t z zakrsu ",zakres_from," do ",zakres_to," to: ",end=" ")
+
+for i in range (zakres_from,zakres_to+1):
 
     if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
-        print (i,end=" ")
+        print ( i,end=" ")
         i+=1
 
 
