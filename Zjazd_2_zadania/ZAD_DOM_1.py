b3c51ac8e048dea48780ed6a7da35dcc608a9ce6
Michał Kuzyk <g.michalkuzyk@gmail.com>
2024-12-02 20:48:13 +0100

    commit

--- Zjazd_2_zadania/ZAD_DOM_1.py
+++ Zjazd_2_zadania/ZAD_DOM_1.py
@@ -5,6 +5,7 @@ Napisz program sumujący pobrane liczby od użytkownika wg wytycznych:
 • program powinien podać sumę liczb parzystych oraz sumę liczb nieparzystych.
 '''
 from ipaddress import summarize_address_range
+from operator import truediv
 
 numbers=[]
 i=0
@@ -19,6 +20,33 @@ while True:
 print("Podałeś ",len(numbers)," liczby")
 print(numbers)
 
-sum_parzyst=0
-sum_nieparzyst=0
+# wersja 1 użycie funkcji sum
+
+sum_parzyste=[]
+sum_nieparzyst=[]
+
+for number in numbers:
+    if number % 2==0:
+        sum_parzyste.append(number)
+    else:
+        sum_nieparzyst.append(number)
+
+print("suma liczb parzystych to: ",sum(sum_parzyste))
+print("suma liczb nieparzystych to: ",sum(sum_nieparzyst))
+
+#wersja 2 bez funkcji
+
+sum_p=0
+sum_n=0
+
+for number in numbers:
+    if number % 2 == 0:
+       sum_p+=number
+    else:
+        sum_n+=number
+print("suma liczb parzystych to: ",str(sum_p),", a suma liczb nieparzystych to: ",str(sum_n))
+
+
+
+
 
