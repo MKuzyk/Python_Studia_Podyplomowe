b63eeb2f6cfb134d4fb7f630dec17abb9557ef8c
Michał Kuzyk <g.michalkuzyk@gmail.com>
2024-12-02 20:21:04 +0100

    commit

--- Zjazd_2_zadania/ZAD_DOM_1.py
+++ Zjazd_2_zadania/ZAD_DOM_1.py
@@ -4,3 +4,21 @@ Napisz program sumujący pobrane liczby od użytkownika wg wytycznych:
 • niepodanie liczby powinno zakończyć wprowadzanie danych,
 • program powinien podać sumę liczb parzystych oraz sumę liczb nieparzystych.
 '''
+from ipaddress import summarize_address_range
+
+numbers=[]
+i=0
+
+while True:
+    number=(input("Podaj "+str(i+1)+" liczbę: "))
+    if number=="":
+        break
+    else:
+        numbers.append(int(number))
+    i+=1
+print("Podałeś ",len(numbers)," liczby")
+print(numbers)
+
+sum_parzyst=0
+sum_nieparzyst=0
+
