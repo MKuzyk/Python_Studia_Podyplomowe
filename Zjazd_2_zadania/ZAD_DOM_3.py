e22c1312e0f7c5e6691f326f75ac53aaf5eff35a
Michał Kuzyk <g.michalkuzyk@gmail.com>
2024-12-06 07:16:12 +0100

    commit

--- Zjazd_2_zadania/ZAD_DOM_3.py
+++ Zjazd_2_zadania/ZAD_DOM_3.py
@@ -2,3 +2,9 @@
 Napisz program pobierający od użytkownika liczbę całkowitą i zwracający
 liczbę jedynek w ciągu bitów reprezentujących tę liczbę.
 '''
+
+number=int(input("Podaj liczbę całkowitą: "))
+
+
+print("liczba jedyne w ciągu bitów reprezentujących tę liczbę t: ")
+print("{:08b}".format(number))
