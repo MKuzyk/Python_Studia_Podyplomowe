70e1502100f975d3b143a5baba09c70e77de63dd
MKuzyk <g.michalkuzyk@gmail.com>
2025-02-09 17:46:43 +0100

    Add files via upload

---
+++ LAB_17_E_1/pack1/subpack_1/mod2.py
@@ -0,0 +1,2 @@
+def fun():
+    print("Jestem funkcja fun() z modułu mod2 z podpakietu subpack_1")
---
+++ LAB_17_E_1/pack1/subpack_2/mod3.py
@@ -0,0 +1,2 @@
+def fun():
+    print("Jestem funkcja fun() z modułu mod3 z podpakietu subpack_2")
