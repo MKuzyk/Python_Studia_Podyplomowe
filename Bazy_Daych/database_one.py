f3a7f6ec42e86443e6d283a9df0975f90289fbb5
Michał Kuzyk <g.michalkuzyk@gmail.com>
2025-04-06 20:11:43 +0200

    t

--- Bazy_Daych/database_one.py
+++ Bazy_Daych/database_one.py
@@ -19,17 +19,31 @@ connection_string = (
 )
 
 connection = pyodbc.connect(connection_string)
-
+'''
 connection.execute("CREATE TABLE users(ID int identity, name varchar(255),age int)")
 connection.execute("Insert into users(name,age) values('Michal',40),('Andrzej',28)")
+connection.commit()
+'''
 
+#connection.execute("Insert into users(name,age) values('Jan',40),('Wojcieh',28)")
+#connection.commit()
 
 cursor=connection.cursor()
 
-cursor.execute("Select * from users")
+nowe_imie = input('Podaj nowe imię: ')
+
+
+cursor.execute(f"Update users set name='{nowe_imie}' where name ='Nowe'")
+connection.commit()
+
+print(f'{cursor.rowcount} wierszy zmienionych')
+
+cursor.execute("Select name from users")
+
+
 
 for row in cursor:
-    print(row)
+    print(row[0])
 
 cursor.close()
 connection.close()
