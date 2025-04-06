745dfd15ab59900ee45b138f18866532714c151f
Michał Kuzyk <g.michalkuzyk@gmail.com>
2025-04-06 18:48:43 +0200

    t

--- Bazy_Daych/database_one.py
+++ Bazy_Daych/database_one.py
@@ -1,6 +1,19 @@
 import pyodbc
 import os
 from dotenv import load_dotenv
+from pyodbc import drivers
+
 load_dotenv()
 
-print(os.environ.get('DB_PASSWORD'))
+db_password = os.environ.get('DB_PASSWORD')
+sushi_login = 'mkuzyk'
+server = 'morfeusz.wszib.edu.pl'
+
+connection_string = (
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    f'SERVER={server};'
+    f'DATABASE={sushi_login};'
+    f'UID={sushi_login};'
+    f'PWD={db_password};'
+    'Encrypt=no'
+)
