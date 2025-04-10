cded3510a9fe5e7c5861512e64997ab51e2d085f
Michał Kuzyk <g.michalkuzyk@gmail.com>
2025-04-10 22:12:29 +0200

    t

--- Bazy_Daych/accounts.py
+++ Bazy_Daych/accounts.py
@@ -1,3 +1,5 @@
+import datetime
+
 from database_one import connection
 
 class Account:
@@ -9,29 +11,49 @@ class Account:
                            (self.name,self.balance))
         cursor = connection.execute("SELECT @@IDENTITY as ID")
 
-        self.id = cursor.fetchval
+        self.id = cursor.fetchval()
 
         connection.commit()
 
         print(f'Konto zostało utworzone dla {self.name} z balansem {self.balance}')
 
-    def deposit(self, amount:float) -> float:
+    def deposit(self, amount:float,auto_commit:bool=True) -> float:
         if amount > 0:
             self.balance +=amount
+            connection.execute("UPDATE accounts SET balance = ? WHERE account_id = ?",(self.balance,self.id))
+            connection.execute("INSERT INTO transactions (account_id,transaction_time,amount) VALUES(?,?,?)",
+                               (self.id,datetime.datetime.now(),amount))
+            if auto_commit:
+                connection.commit()
+
             print(f'Na konto {self.name} zostało dodane {amount} PLN')
         return round(self.balance,2)
 
-    def withdraw(self, amount:float) -> float:
-        if amount > 0:
+    def withdraw(self, amount:float,auto_commit:bool=True) -> float:
+        if 0 < amount <= self.balance:
             self.balance -=amount
+
+            connection.execute("UPDATE accounts SET balance = ? WHERE account_id = ?", (self.balance, self.id))
+            connection.execute("INSERT INTO transactions (account_id,transaction_time,amount) VALUES(?,?,?)",
+                               (self.id, datetime.datetime.now(), -amount))
+            if auto_commit:
+                connection.commit()
+
             print(f'Z konta {self.name} zostało wypłacone {amount} PLN')
+        else:
+            raise ValueError('Nie masz wystarczających środków na koncie')
         return round(self.balance,2)
 
+
+    def send_funds(self, amount: float, account):
+        self.withdraw(amount,auto_commit=False)
+        account.deposit(amount,auto_commit=False)
+        connection.commit()
+
 if __name__ == '__main__':
-    account=Account ('Michal')
-    account.deposit(10)
-    account.deposit(0.1)
-    balance = account.withdraw(5)
-    print(balance)
+    account_jan = Account('Jan',10)
+    account_michal = Account('Michal', 10)
+    account_jan.send_funds(5,account_michal)
+
 
 
