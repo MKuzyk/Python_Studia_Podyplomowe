8ea0e2803146182c32af950fb02f0e8a40e91bce
Michał Kuzyk <g.michalkuzyk@gmail.com>
2025-04-10 21:09:36 +0200

    t

--- Bazy_Daych/accounts.py
+++ Bazy_Daych/accounts.py
@@ -1,7 +1,18 @@
+from database_one import connection
+
 class Account:
     def __init__(self,name: str,opening_balance: float = 0.0):
         self.name=name
         self.balance=opening_balance
+
+        connection.execute("INSERT into accounts(name,balance) VALUES(?,?)",
+                           (self.name,self.balance))
+        cursor = connection.execute("SELECT @@IDENTITY as ID")
+
+        self.id = cursor.fetchval
+
+        connection.commit()
+
         print(f'Konto zostało utworzone dla {self.name} z balansem {self.balance}')
 
     def deposit(self, amount:float) -> float:
