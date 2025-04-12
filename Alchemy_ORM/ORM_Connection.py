298c5756c4651fd952d21b5feb6159ff0176b829
Michal <g.michalkuzyk@gmail.com>
2025-04-12 21:38:07 +0200

    commit

--- Alchemy_ORM/ORM_Connection.py
+++ Alchemy_ORM/ORM_Connection.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from AlchemyORM.alchemy_orm import Base
+from Alchemy_ORM.alchemy_orm import Base
 
 load_dotenv()
 
